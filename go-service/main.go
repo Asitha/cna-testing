f5cd46346b9aa55bfb630ea2ee005bb0f8a3fc92
Asitha Nanayakkara <daun07@gmail.com>
2023-10-31 11:36:26 +0530

    Read from config file and env var

--- go-service/main.go
+++ go-service/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
-	serverMux := http.NewServeMux();
+	serverMux := http.NewServeMux()
 	serverMux.HandleFunc("/greeter/greet", greet)
-
 	err := http.ListenAndServe(":9090", serverMux)
 	if err != nil {
 		panic(err)
@@ -16,9 +16,15 @@ func main() {
 }
 
 func greet(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Query().Get("name")
-	if name == "" {
-		name = "Stranger"
+	// read from a file a line
+	greeterText := os.Getenv("GREETING_TEXT")
+	name, err := os.ReadFile("name.txt")
+	if err != nil {
+		fmt.Printf("Error reading file: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Error reading file")
+		return
 	}
-	fmt.Fprintf(w, "Hello, %s!\n", name)
+
+	fmt.Fprintf(w, "%s, %s!\n", greeterText, string(name))
 }
